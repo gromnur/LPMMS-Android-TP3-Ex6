cb5a7f814eacc05084f9737708d0e06b01657dfe
Flavien Regis <flavienregis@gmail.com>
2018-12-12 14:17:10 +0100

    Debut Ajout style

--- app/src/main/java/iut/flavienregis/convertisseurdistance/MainActivity.java
+++ app/src/main/java/iut/flavienregis/convertisseurdistance/MainActivity.java
@@ -1,15 +1,9 @@
 package iut.flavienregis.convertisseurdistance;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
-import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
-
-    // Récuperation des Edit Text
-    EditText etPied = (EditText) findViewById(R.id.etPied);
-    EditText etCentimetre = (EditText) findViewById(R.id.etCentimetre);
-    EditText etPouces = (EditText) findViewById(R.id.etPouces);
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
